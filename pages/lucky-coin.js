5712e906e23f32c8133e00897e301fd89d6f260c
Ziddan Dwi Putra <ziddandwiputra21@gmail.com>
2022-04-15 10:57:59 +0700

    add font, manifest, icon, and layouting

--- pages/lucky-coin.js
+++ pages/lucky-coin.js
@@ -1,7 +1,7 @@
-export default function Home(){
+export default function LuckyCoin(){
     return(
         <div className="container">
-            Index
+            Lucky Coin
         </div>
     )
 }
